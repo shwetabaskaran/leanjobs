54e02b65583878d2c6697a5b19be2e3cfcff9c00
Rishi <rishikesh.a.j@gmail.com>
2019-04-07 17:13:30 -0500

    Added Questionnaire Response screen - Logic pending

--- leanjobs/app/src/main/java/com/example/leanjobs/ApplicantDetails.java
+++ leanjobs/app/src/main/java/com/example/leanjobs/ApplicantDetails.java
@@ -1,13 +1,31 @@
 package com.example.leanjobs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ApplicantDetails extends AppCompatActivity {
 
+    Button btnQuestionResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_applicant_details);
+        btnQuestionResponse = (Button) findViewById(R.id.buttonQuestionResponse);
+        btnQuestionResponse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),
+                        QuestionnaireResponse.class);
+                startActivity(i);
+                finish();
+//                Toast.makeText(getApplicationContext(),
+//                        "Admin Login Screen", Toast.LENGTH_LONG)
+//                        .show();
+            }
+        });;
     }
 }
---
+++ leanjobs/app/src/main/java/com/example/leanjobs/QuestionnaireResponse.java
@@ -0,0 +1,13 @@
+package com.example.leanjobs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionnaireResponse extends AppCompatActivity {
+    //Write Loginc for Dynamic list of cards https://javapapers.com/android/android-cards-list-view/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire_response);
+    }
+}
