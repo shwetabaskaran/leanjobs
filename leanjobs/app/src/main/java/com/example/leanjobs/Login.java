9a79e304699e6e070ae6482c82b390f06360ed83
Shweta <shwetabaskaran@gmail.com>
2019-04-10 12:24:16 -0500

    HTTP get request from Login screen

--- leanjobs/app/src/main/java/com/example/leanjobs/Login.java
+++ leanjobs/app/src/main/java/com/example/leanjobs/Login.java
@@ -1,19 +1,35 @@
 package com.example.leanjobs;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Login extends AppCompatActivity {
 
 
-    Button btnadmlogin, btnforgotpassword, btnsignup;
+    Button btnadmlogin, btnforgotpassword, btnsignup, btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +37,9 @@ public class Login extends AppCompatActivity {
         btnsignup = (Button) findViewById(R.id.btnsignup);
         btnadmlogin = (Button) findViewById(R.id.btnadmlogin);
         btnforgotpassword = (Button) findViewById(R.id.btnforgotpassword);
+        btnlogin = (Button) findViewById(R.id.btnLogin);
+
+
         btnadmlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,7 +82,81 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button btnlogin = (Button)findViewById(R.id.btnLogin);
+                btnlogin.setClickable(false);
+                new LongRunningGetIO().execute();
+            }
 
+            class LongRunningGetIO extends AsyncTask<Void, Void, String> {
 
+                protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+                    InputStream in = entity.getContent();
+                    StringBuffer out = new StringBuffer();
+                    int n = 1;
+                    while (n > 0) {
+                        byte[] b = new byte[4096];
+                        n = in.read(b);
+                        if (n > 0) out.append(new String(b, 0, n));
+                    }
+                    return out.toString();
+                }
+
+                @SuppressLint("WrongThread")
+                @Override
+                protected String doInBackground(Void... params) {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpContext localContext = new BasicHttpContext();
+                    HttpGet httpGet = new HttpGet("http://dhillonds.com/leanjobsweb/index.php/api/example/users");
+                    String text = null;
+                    try {
+                        HttpResponse response = httpClient.execute(httpGet, localContext);
+                        HttpEntity entity = response.getEntity();
+                        text = getASCIIContentFromEntity(entity);
+                    } catch (Exception e) {
+                        return e.getLocalizedMessage();
+                    }
+                    return text;
+                }
+
+                protected void onPostExecute(String results) {
+                    if (results != null) {
+                        String JSONresults = results;
+                        try {
+                            JSONArray users = new JSONArray(JSONresults);
+                            EditText edEmail = (EditText) findViewById(R.id.email);
+                            EditText edPass = (EditText) findViewById(R.id.password);
+                            String em = edEmail.getText().toString();
+                            String pass = edPass.getText().toString();
+
+                            for(int i = 0; i<users.length(); i++)
+                            {
+                                JSONObject obj = users.getJSONObject(i);
+                                String email = obj.getString("email");
+                                String password = obj.getString("name");
+                                if(email.equals(em) && (password.equals(pass))){
+                                    Intent intent = new Intent(getApplicationContext(),
+                                            UserProfile.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                    Toast.makeText(getApplicationContext(),
+                                            "Login successful", Toast.LENGTH_SHORT)
+                                            .show();
+                                    break;
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Button btnLogin = (Button) findViewById(R.id.btnLogin);
+                    btnLogin.setClickable(true);
+                }
+            }
+        });
     }
 }
