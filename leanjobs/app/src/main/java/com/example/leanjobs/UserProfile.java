2bd64b65c72407b81ddf6c2305d626ccb680aba9
Rishi <rishikesh.a.j@gmail.com>
2019-04-21 01:12:31 -0500

    Added code for profile picture and pdf

--- leanjobs/app/src/main/java/com/example/leanjobs/UserProfile.java
+++ leanjobs/app/src/main/java/com/example/leanjobs/UserProfile.java
@@ -1,12 +1,22 @@
 package com.example.leanjobs;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -23,7 +33,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserProfile extends AppCompatActivity {
-    Button btnSave;
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView ProfilePic;
+    final int PERMISSION_REQUEST_CAMERA = 103;
+    Button btnSave,UploadPDF;
     EditText fullName,emailAddress,mobileNumber;
     String URLPost="http://dhillonds.com/leanjobsweb/index.php/api/users/update_profile";
     @Override
@@ -34,12 +47,15 @@ public class UserProfile extends AppCompatActivity {
         fullName = findViewById(R.id.edituserFullName);
         emailAddress = findViewById(R.id.edituserEmail);
         mobileNumber = findViewById(R.id.edituserMobile);
+        this.ProfilePic = (ImageView)this.findViewById(R.id.userprofilePic);
 
         fullName.setText(getSharedPreferences("UserDataPreferences", Context.MODE_PRIVATE).getString("full_name",""));
         mobileNumber.setText(getSharedPreferences("UserDataPreferences", Context.MODE_PRIVATE).getString("phone_num",""));
         emailAddress.setText(getSharedPreferences("UserDataPreferences", Context.MODE_PRIVATE).getString("email",""));
 
         emailAddress.setEnabled(false);
+        UploadPDF = (Button) findViewById(R.id.userprofileUploadResume);
+        initImageButton();
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +66,82 @@ public class UserProfile extends AppCompatActivity {
                 }
             }
         });
+
+        UploadPDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent();
+                intent.setType("application/pdf");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select PDF"), 1);
+            }
+        });
     }
+
+
+
+    private void initImageButton() {
+        ImageButton ib = (ImageButton) findViewById(R.id.camera);
+        ib.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (ContextCompat.checkSelfPermission(UserProfile.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(UserProfile.this, android.Manifest.permission.CAMERA)) {
+                            Snackbar.make(findViewById(R.id.activity_signup), "The app needs permission to take pictures.", Snackbar.LENGTH_INDEFINITE).setAction("Ok", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                    ActivityCompat.requestPermissions(UserProfile.this, new String[]{ android.Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+                                }
+                            }).show();
+                        } else {
+                            ActivityCompat.requestPermissions(UserProfile.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+                        }
+                    }
+                    else {
+                        takePhoto();
+                    }
+                } else {
+                    takePhoto();
+                }
+            }
+        });
+    }
+    public void takePhoto(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    Bitmap scaledPhoto = Bitmap.createScaledBitmap(photo, 144, 144, true);
+                    ProfilePic.setImageBitmap(scaledPhoto);
+
+                }
+                catch (Exception ex){
+                    Toast.makeText(UserProfile.this, ex.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto();
+                } else {
+                    Toast.makeText(UserProfile.this, "You will not be able to save contact pictures from this app", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
     private void PostSignUpData() {
         StringRequest stringRequest = new StringRequest(Request.Method.POST,URLPost, new Response.Listener<String>(){
             @Override
